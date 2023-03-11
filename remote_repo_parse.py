eba5cb2a8e2bebb035e00df17e47586eb5d639c8
Victoria Brunner <astrobrunner@gmail.com>
2023-03-11 17:19:06 -0500

    add extra Bitbucket functionality for cloning using git@bitbcket.org syntax

--- remote_repo_parse.py
+++ remote_repo_parse.py
@@ -3,6 +3,6 @@
 import os
 import sys
 
-remote_repo_string = sys.argv[1].replace("https://github.com/", "").replace("https://gitlab.com/", "").replace("https://bitbucket.org/", "").replace("/src/main", "").replace(".git", "")
+remote_repo_string = sys.argv[1].replace("https://github.com/", "").replace("https://gitlab.com/", "").replace("https://bitbucket.org/", "").replace("/src/main", "").replace(".git", "").replace("git@bitbucket.org:", "")
 remote_repo_list = remote_repo_string.split("/")
 print(remote_repo_list[1])
